d4d909e3a7722fc2535658b7dcd0732de0271dbe
Max Klenk <max@klenk.biz>
2024-01-22 02:26:05 +0100

    chore: update yarn.lock

--- src/chains/utils.ts
+++ src/chains/utils.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from 'ethers'
+import { BigNumber } from '@ethersproject/bignumber'
 
 export const prefixChainId = (chainId: number): string => {
   return '0x' + BigNumber.from(chainId)._hex.split('0x')[1].replace(/\b0+/g, '')
