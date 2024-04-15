e5ec629c78348f090bdbb4c820a5e7ff1ece16ac
Alex1237 <54893307+myz1237@users.noreply.github.com>
2024-04-15 21:26:30 +0800

    feat: add mode coins and chain (#29)
    Co-authored-by: myz1237 <myz1237@myz1237deMacBook-Pro.local>

--- src/coins/coins.ts
+++ src/coins/coins.ts
@@ -126,6 +126,10 @@ export const basicCoins: BasicCoin[] = [
         address: '0x0000000000000000000000000000000000000000',
         decimals: 18,
       },
+      [ChainId.MOD]: {
+        address: '0x0000000000000000000000000000000000000000',
+        decimals: 18,
+      },
 
       // Testnets
       [ChainId.ONET]: {
@@ -627,6 +631,10 @@ export const basicCoins: BasicCoin[] = [
         address: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
         decimals: 6,
       },
+      [ChainId.MOD]: {
+        address: '0xf0F161fDA2712DB8b566946122a5af183995e2eD',
+        decimals: 6,
+      },
     },
   },
 
@@ -896,6 +904,10 @@ export const basicCoins: BasicCoin[] = [
       },
       [ChainId.SCL]: {
         address: '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4',
+        decimals: 6,
+      },
+      [ChainId.MOD]: {
+        address: '0xd988097fb8612cc24eeC14542bC03424c656005f',
         decimals: 6,
       },
 
@@ -1235,6 +1247,10 @@ export const basicCoins: BasicCoin[] = [
       },
       [ChainId.SCL]: {
         address: '0x5300000000000000000000000000000000000004',
+        decimals: 18,
+      },
+      [ChainId.MOD]: {
+        address: '0x4200000000000000000000000000000000000006',
         decimals: 18,
       },
 
@@ -1915,6 +1931,17 @@ export const wrappedTokens: { [ChainId: string]: StaticToken } = {
     name: 'WETH',
     logoURI:
       'https://static.debank.com/image/era_token/logo_url/0x5aea5775959fbc2557cc8789bc1bf90a239d9a91/61844453e63cf81301f845d7864236f6.png',
+  },
+  [ChainId.MOD]: {
+    // https://explorer.mode.network/token/0x4200000000000000000000000000000000000006
+    address: '0x4200000000000000000000000000000000000006',
+    symbol: 'WETH',
+    decimals: 18,
+    chainId: ChainId.MOD,
+    coinKey: CoinKey.WETH,
+    name: 'WETH',
+    logoURI:
+      'https://static.debank.com/image/mtr_token/logo_url/0x79a61d3a28f8c8537a3df63092927cfa1150fb3c/61844453e63cf81301f845d7864236f6.png',
   },
 
   // Testnets
