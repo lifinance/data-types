aeacb0f5c526180e1a3c0ef8e1a2e1d34a72154a
myz1237 <mengyizhuoprivate@outlook.com>
2024-12-06 17:43:02 +0800

    fix: incorrect address on wld on wcc

--- src/coins/coins.ts
+++ src/coins/coins.ts
@@ -1832,7 +1832,7 @@ export const basicCoins: BasicCoin[] = [
         decimals: 18,
       },
       [ChainId.WCC]: {
-        address: '0x79A02482A880bCE3F13e09Da970dC34db4CD24d1',
+        address: '0x2cFc85d8E48F8EAB294be644d9E25C3030863003',
         decimals: 18,
       },
     },
