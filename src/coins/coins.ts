7fb06abaf475f7a2500a1a86ec25b97dadd2e38e
myz1237 <mengyizhuoprivate@outlook.com>
2025-01-14 16:22:28 +0900

    fix: wrong wrapped token symbol

--- src/coins/coins.ts
+++ src/coins/coins.ts
@@ -2416,12 +2416,12 @@ export const wrappedTokens: { [ChainId: string]: StaticToken } = {
   },
   [ChainId.SON]: {
     // https://sonicscan.org/token/0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38
-    address: '0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38',
-    symbol: 'WETH',
+    address: '0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38',
+    symbol: 'wS',
     decimals: 18,
     chainId: ChainId.SON,
-    coinKey: CoinKey.WETH,
-    name: 'WETH',
+    coinKey: CoinKey.wS,
+    name: 'wS',
     logoURI:
       'https://static.debank.com/image/sonic_token/logo_url/0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38/b4cc70d040518a88adac18d906fcbfff.png',
   },
