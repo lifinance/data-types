6902eca49ff11d12f13d4e2b1d5812624d8fa1c4
Daniela Chybisova <iron.melmel@gmail.com>
2026-04-02 09:49:09 +0200

    chore: add missing tokens

--- src/coins/coins.ts
+++ src/coins/coins.ts
@@ -1168,6 +1168,14 @@ export const basicCoins: BasicCoin[] = [
         address: '0x5fd84259d66Cd46123540766Be93DFE6D43130D7',
         decimals: 6,
       },
+      [ChainId.ARBS]: {
+        address: '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d',
+        decimals: 6,
+      },
+      [ChainId.BAST]: {
+        address: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
+        decimals: 6,
+      },
     },
   },
   // USDC.e
