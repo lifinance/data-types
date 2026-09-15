af7b9558079c2c20b6fefe38b6e6ead291cf1aa5
Daniela Chybisova <iron.melmel@gmail.com>
2026-09-16 00:42:39 +0200

    chore: update Arc RPC and explorer (#274)

--- src/chains/supportedChains.evm.ts
+++ src/chains/supportedChains.evm.ts
@@ -2097,7 +2097,11 @@ export const supportedEVMChains: EVMChain[] = [
         symbol: 'USDC',
         decimals: 18,
       },
-      rpcUrls: ['https://arc-rpc.transferto.xyz/'],
+      rpcUrls: [
+        'https://rpc.drpc.mainnet.arc.io',
+        'https://rpc.mainnet.arc.io',
+        'https://rpc.quicknode.mainnet.arc.io',
+      ],
     },
   },
 
