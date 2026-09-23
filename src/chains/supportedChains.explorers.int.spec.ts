b80bf0ce35b0dd5e2ad70091e89c679b3a509985
Eugene Chybisov <imchybisov@gmail.com>
2026-09-23 13:30:46 +0200

    chore: move to TypeScript 7 and stop publishing spec files

---
+++ src/chains/supportedChains.explorers.int.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from 'vitest'
+import { supportedChains } from './supportedChains.js'
+
+// This public explorer test works for all supported chains (EVM and non-EVM) because
+// all chains share a `metamask` object structured the same way, if that's to change
+// this test will have to be adapted per VM like the RPC tests.
+describe.concurrent('validate blockchain explorers', () => {
+  supportedChains.forEach((chain) => {
+    expect(chain.metamask.blockExplorerUrls.length).toBeGreaterThan(0)
+  })
+
+  const explorerUrls = supportedChains.flatMap((chain) =>
+    chain.metamask.blockExplorerUrls.map((blockExplorerUrl) => ({
+      blockExplorerUrl,
+      chainKey: chain.key,
+    }))
+  )
+
+  test.for(explorerUrls)(
+    'should get a valid response from $chainKey explorer: $blockExplorerUrl',
+    { timeout: 10_000, retry: 3 },
+    async ({ blockExplorerUrl }) => {
+      const response = await fetch(blockExplorerUrl, {
+        method: 'GET',
+      })
+
+      // some explorers have advanced bot protections, best we can do is
+      // check for any valid TCP response before timeout
+      expect(response).toBeDefined()
+    }
+  )
+})
